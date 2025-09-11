7b54917fc08ede7062428c4bcafb9964836351b2
mnbvmnbv2 <ferdinand@ytteborg.com>
2025-09-12 00:48:34 +0200

    solve 7

---
+++ python/7/main.py
@@ -0,0 +1,13 @@
+def main(n: int = 10001) -> int:
+    primes = []
+    to_check = 1
+    while len(primes) < n:
+        to_check += 1
+        is_prime = True
+        for i in primes:
+            if to_check % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(to_check)
+    return primes[-1]
