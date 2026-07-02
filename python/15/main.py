9a7b7db0b500dadc1f4763816c80c319fffd5b23
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-02 21:40:35 +0200

    remove main from 15

--- python/15/main.py
+++ python/15/main.py
@@ -3,7 +3,3 @@ from math import factorial
 
 def main(num: int = 20) -> int:
     return int(factorial(num * 2) / (factorial(num) * factorial(num)))
-
-
-if __name__ == "__main__":
-    print(main())
