e6259cc3737d41f25c98f697c0310821525104b2
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-06 23:59:36 +0200

    smaller 22

--- python/22/main.py
+++ python/22/main.py
@@ -2,11 +2,6 @@ from pathlib import Path
 
 
 def main() -> int:
-    names = Path("python/22/names.txt").read_text().replace('"', "").split(",")
-    names.sort()
-    total = 0
-    for idx, name in enumerate(names):
-        name_val = sum((ord(c) - 64) for c in name)
-        total += name_val * (idx + 1)
+    names = sorted(Path("python/22/names.txt").read_text().replace('"', "").split(","))
 
-    return total
+    return sum(sum((ord(c) - 64) for c in n) * (i + 1) for i, n in enumerate(names))
