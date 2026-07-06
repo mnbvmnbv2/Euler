f4a740af555c9747e8e52ff2ec978d32ebac7cc1
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-06 22:43:55 +0200

    datetime simpler

--- python/19/lib.py
+++ python/19/lib.py
@@ -2,14 +2,11 @@ import datetime
 
 
 def main() -> int:
-    c = 0
-    day = datetime.date(1901, 1, 1)
-    print(day)
-    while day < datetime.date(2001, 1, 1):
-        day += datetime.timedelta(1)
-        if day.weekday() == 6 and day.isoformat().split("-")[-1] == "01":
-            c += 1
-    return c
+    return sum(
+        datetime.date(year, month, 1).weekday() == 6
+        for year in range(1901, 2001)
+        for month in range(1, 13)
+    )
 
 
 if __name__ == "__main__":
