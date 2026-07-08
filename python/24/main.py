189b48c2480d926f9145cf4e753d58904490c745
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-08 21:08:09 +0200

    correct output 24

--- python/24/main.py
+++ python/24/main.py
@@ -4,5 +4,5 @@ import itertools
 def main() -> int:
     for i, p in enumerate(itertools.permutations(range(10), r=10)):
         if i == 999_999:
-            return p
+            return int("".join(str(a) for a in p))
     return 1
