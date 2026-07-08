080328290720788ca3cf522e78dd55f432198785
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-08 20:51:45 +0200

    faster

--- python/23/main.py
+++ python/23/main.py
@@ -1,26 +1,29 @@
 def main() -> int:
     sums = [0] * (28124 + 1)
-
     for i in range(1, 28124 // 2):
         for j in range(i * 2, 28124 + 1, i):
             sums[j] += i
 
-    sum_diff = []
-
-    abundant = []
-    for i, s in enumerate(sums):
-        d = s - i
-        sum_diff.append(d)
-        if d > 0:
-            abundant.append(i)
-
     abundant_sums = [0] * 28125
 
-    for idx, i in enumerate(abundant):
-        for j in range(idx, len(abundant)):
-            d = i + abundant[j]
-            if d > 28124:
+    abundant = []
+    abundant_flag = [0] * 28125
+
+    for i, s in enumerate(sums):
+        if s - i > 0:
+            abundant_flag[i] = 1
+            abundant.append(i)
+
+    for i in range(28124):
+        for j in abundant:
+            if j > i:
                 break
-            abundant_sums[d] = 1
+            if abundant_flag[j] and abundant_flag[i - j]:
+                abundant_sums[i] = 1
+                break
 
     return sum(i for i in range(28124) if abundant_sums[i] == 0)
+
+
+if __name__ == "__main__":
+    main()
