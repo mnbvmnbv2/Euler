15c344566fd72eb7725589268501e94056c98841
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-08 23:04:12 +0200

    cleanup

--- python/24/main.py
+++ python/24/main.py
@@ -2,16 +2,13 @@ import math
 
 
 def main() -> int:
-
     n = 999_999
-    digits = []
-    to_pick = list(range(10))
+    result = []
+    digits = list(range(10))
 
     for i in range(9, -1, -1):
         num_pos = math.factorial(i)
         a, n = divmod(n, num_pos)
-        picked = to_pick[a]
-        digits.append(picked)
-        to_pick.remove(picked)
+        result.append(digits.pop(a))
 
-    return int("".join(map(str, digits)))
+    return int("".join(map(str, result)))
