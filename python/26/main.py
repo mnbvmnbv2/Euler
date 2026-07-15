f43d9655db171ed5831f137ab359eece641fe7c6
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-16 00:57:35 +0200

    remove unused

--- python/26/main.py
+++ python/26/main.py
@@ -3,22 +3,13 @@ def main() -> int:
     longest_idx = 0
     for i in range(1000, 1, -1):
         m = 1
-        n = i
-        j = 0
-        seri = []
         rems = []
         while True:
-            j += 1
-            whole, rem = divmod(m, n)
-            # print(f"{m}/{n}", whole, rem)
+            whole, rem = divmod(m, i)
             m = rem * 10
-            seri.append(whole)
-            # n = rem
-            # or repeat
             if rem in rems:
                 x = rems.index(rem)
                 leng = len(rems) - x
-                # print(leng, x, rem)
                 break
             rems.append(rem)
         if leng > longest:
