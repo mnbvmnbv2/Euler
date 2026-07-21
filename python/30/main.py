24afe1a265f00f1c9907bdc62e754df4284c3e9b
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-21 23:41:49 +0200

    better explanation on 30

--- python/30/main.py
+++ python/30/main.py
@@ -1,7 +1,7 @@
 def main() -> int:
     n = []
-    # seems 200k was the last, not sure why
-    for i in range(2, 200_000):
+    # 9**5 * x until x > 9**5 * x, x = 6
+    for i in range(2, 9**5 * 6):
         if i == sum(a**5 for a in map(int, str(i))):
             n.append(i)
     return sum(n)
