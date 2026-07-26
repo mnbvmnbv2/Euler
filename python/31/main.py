0c1874f69c12c4f2048d3deae8e204d0ac7a3db4
mnbvmnbv2 <ferdinand@ytteborg.com>
2026-07-26 03:36:13 +0200

    simplify

--- python/31/main.py
+++ python/31/main.py
@@ -2,15 +2,13 @@
 
 
 def ways_to_make(n, coins, having) -> int:
-    if not coins:
-        return 0
     if n == 0:
         # ways.append(having)
         return 1
     for coin in reversed(coins):
         if coin <= n:
-            choose = ways_to_make(n, [c for c in coins if c != coin], having)
-            no_choose = ways_to_make(n - coin, coins, having + [coin])
+            no_choose = ways_to_make(n, [c for c in coins if c != coin], having)
+            choose = ways_to_make(n - coin, coins, having + [coin])
             return choose + no_choose
     return 0
 
